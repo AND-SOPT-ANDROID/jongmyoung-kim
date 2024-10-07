d121576ee974d7170b17bde63179d5b19319ea84
Jong <jm991014@gmail.com>
2024-10-07 18:53:52 +0900

    Add/#2: Color Palette

--- app/src/main/java/org/sopt/and/ui/theme/Color.kt
+++ app/src/main/java/org/sopt/and/ui/theme/Color.kt
@@ -9,3 +9,18 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+// Color palette
+val Background = Color(0xFF1B1B1B)
+val Container = Color(0xFF252525)
+val ExtraDarkGray = Color(0xFF2F2F2F)
+val DarkGray = Color(0xFF717171)
+val LightGray = Color(0xFFA5A5A5)
+val ErrorText = Color(0xFFFF27A3)
+
+// Logo Colors
+val WavveMain = Color(0xFF0050FF)
+val TWorld = Color(0xFF3617CE)
+val Naver = Color(0xFF1ec800)
+val Kakao = Color(0xFFFEE500)
+val Facebook = Color(0xFF4267B3)
