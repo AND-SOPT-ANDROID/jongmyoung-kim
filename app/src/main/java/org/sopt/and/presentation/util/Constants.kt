b129f0176a43ad2523dd05ec0217c9b5ab0242b5
Jong <jm991014@gmail.com>
2024-10-11 01:57:07 +0900

    Mod/#2: 문자열 추출 및 Constatns 생성

---
+++ app/src/main/java/org/sopt/and/presentation/util/Constants.kt
@@ -0,0 +1,10 @@
+package org.sopt.and.presentation.util
+
+class Constants {
+    companion object {
+        const val MAX_EMAIL = 50
+        const val MIN_EMAIL = 5
+        const val MAX_PASSWORD = 20
+        const val MIN_PASSWORD = 8
+    }
+}
