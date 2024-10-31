c0165214c3926afaa5c6bce7b4607d21f8d66dd9
Jong <jm991014@gmail.com>
2024-11-01 03:01:42 +0900

    Chore/#6: Theme.kt 파일 수정
    - ime padding 수정 후 변경사항

--- app/src/main/java/org/sopt/and/presentation/theme/Theme.kt
+++ app/src/main/java/org/sopt/and/presentation/theme/Theme.kt
@@ -6,8 +6,6 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
@@ -32,8 +30,6 @@ fun ANDANDROIDTheme(
     if (!view.isInEditMode) {
         SideEffect {
             val window = (view.context as Activity).window
-            window.statusBarColor = Color.Transparent.toArgb()
-            window.navigationBarColor = Color.Transparent.toArgb()
             WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
         }
     }
