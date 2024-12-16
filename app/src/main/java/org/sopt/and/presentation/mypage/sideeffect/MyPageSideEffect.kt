f29ddedd78ac0adc33249654b6d5463d345e84d5
Jong <jm991014@gmail.com>
2024-12-16 23:38:57 +0900

    Del/#12: 불필요 파일 삭제

--- app/src/main/java/org/sopt/and/presentation/mypage/sideeffect/MyPageSideEffect.kt
+++
@@ -1,6 +0,0 @@
-package org.sopt.and.presentation.mypage.sideeffect
-
-sealed class MyPageSideEffect {
-    data object NavigateToSignIn : MyPageSideEffect()
-    class Toast(val message: String) : MyPageSideEffect()
-}
