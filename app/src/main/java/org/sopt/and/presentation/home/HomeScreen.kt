f5ff2b1e66cb79b16acd72c38984171bf3f6d7dc
Jong <jm991014@gmail.com>
2024-12-16 23:40:28 +0900

    Refactor/#12: Home 화면 MVI 리팩토링

--- app/src/main/java/org/sopt/and/presentation/home/HomeScreen.kt
+++ app/src/main/java/org/sopt/and/presentation/home/HomeScreen.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
